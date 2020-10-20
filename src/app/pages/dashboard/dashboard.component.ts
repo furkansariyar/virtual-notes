96c162f1d22a68fb22049d95510a2bf30087c2fe
Furkan Sarıyar <sariyarfurkan@gmail.com>
2020-10-20 19:23:28 +0300

    sidebar is added in dashboard page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,7 +4,8 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: "app-dashboard",
-  templateUrl: "dashboard.component.html"
+  templateUrl: "dashboard.component.html",
+  styleUrls: ["dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
   public canvas : any;
